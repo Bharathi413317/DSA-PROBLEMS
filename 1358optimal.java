7af295630006537ed9f99f048093cf127a72bda4
bharathi bothsa <bothsabharathi1@gmail.com>
2026-06-30 23:11:00 +0530

    Implement substring counting in numberOfSubstrings method

---
+++ 1358optimal.java
@@ -0,0 +1,17 @@
+//optimal
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int ans=0;
+        int left=0;
+        int n=s.length();
+        int []freq=new int[3];
+        for(int right=0;right<s.length();right++){
+            freq[s.charAt(right)-'a']++;
+            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
+                ans+=n-right;
+                freq[s.charAt(left)-'a']--;
+                left++;
+            }
+        }return ans;
+    }
+}
