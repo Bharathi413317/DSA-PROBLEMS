90dcc4600924c3a92ef09d07381aff8f8cf75f9c
bharathi botsa <bothsabharathi1@gmail.com>
2026-09-03 19:53:18 +0530

    Time: 2 ms (100%), Space: 121.6 MB (74.64%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -5,11 +5,8 @@ class Solution {
         for (int num : nums1) {
             min = Math.min(min, num);
         }
-        
-        
         if (min % 2 == 1) return true;
         
-       //if(min%2==1) return true;
         for (int num : nums1) {
             if (num % 2 == 1) {
                 return false;
