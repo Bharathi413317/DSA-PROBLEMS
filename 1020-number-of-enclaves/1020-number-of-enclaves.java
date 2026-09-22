71b6c61c406b160f8cd3f2e8269ca4151e7375bb
bharathi botsa <bothsabharathi1@gmail.com>
2026-09-22 21:47:28 +0530

    Time: 10 ms (31.78%), Space: 53.4 MB (88.62%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,44 +1,57 @@
-class Solution {
-    public void dfs(int row,int col,int [][]vis,int [][]grid){
-        vis[row][col]=1;
-        int n=grid.length;
-        int m=grid[0].length;
-        int []delrow={-1,0,+1,0};
-        int delcol[]={0,+1,0,-1};
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                dfs(nrow,ncol,vis,grid);
-            }
-        }
+class Node{
+    int first;
+    int second;
+    public Node(int first,int second){
+        this.first=first;
+        this.second=second;
     }
+}
+class Solution {
     public int numEnclaves(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         int [][]vis=new int[n][m];
+        int cnt=0;
+        Queue<Node>q=new LinkedList<Node>();
         for(int j=0;j<m;j++){
-            if(vis[0][j]==0 && grid[0][j]==1){
-                dfs(0,j,vis,grid);
+            if(grid[0][j]==1 && vis[0][j]==0){
+                q.add(new Node(0,j));
+                vis[0][j]=1;
             }
-            if(vis[n-1][j]==0 && grid[n-1][j]==1){
-                dfs(n-1,j,vis,grid);
+            if(grid[n-1][j]==1 && vis[n-1][j]==0){
+                q.add(new Node(n-1,j));
+                vis[n-1][j]=1;
             }
-        }
-        for(int i=0;i<n;i++){
-            if(vis[i][0]==0 && grid[i][0]==1){
-                dfs(i,0,vis,grid);
-            }if(vis[i][m-1]==0 && grid[i][m-1]==1){
-                dfs(i,m-1,vis,grid);
+        }for(int i=0;i<n;i++){
+            if(grid[i][0]==1 && vis[i][0]==0){
+                q.add(new Node(i,0));
+                vis[i][0]=1;
+            }if(grid[i][m-1]==1 && vis[i][m-1]==0){
+                q.add(new Node(i,m-1));
+                vis[i][m-1]=1;
             }
-        }int count=0;
-        for(int i=0;i<n;i++){
+        }while (!q.isEmpty()){
+            int row=q.peek().first;
+            int col=q.peek().second;
+            q.remove();
+            int []delrow={-1,0,+1,0};
+            int []delcol={0,+1,0,-1};
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.add(new Node(nrow,ncol));
+                    vis[nrow][ncol]=1;
+                }
+
+            }
+        }for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]==1){
-                    count++;
-                   
+                    cnt++;
                 }
             }
-        }return count;
+        }return cnt;
+
     }
 }
