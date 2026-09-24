5f4d90b8f5f31dd8b3df10d193a1eae328c369e8
bharathi botsa <bothsabharathi1@gmail.com>
2026-09-24 14:21:54 +0530

    Time: 0 ms (100%), Space: 47 MB (87.84%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,42 +1,25 @@
 class Solution {
-    public boolean bfs(int row,int []vis,int []color,int [][]graph){
-        vis[row]=1;
-        color[row]=0;
-        Queue<Integer>q=new LinkedList<>();
-        q.add(row);
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-               int n=q.poll();
-               for(int j=0;j<graph[n].length;j++){
-                int num=graph[n][j];
-                if(vis[num]==0){
-                    if(color[n]==0){
-                        vis[num]=1;
-                        color[num]=1;
-                        q.add(num);
-                    }else{
-                        vis[num]=1;
-                        color[num]=0;
-                        q.add(num);
-                    }
-                }else{
-                    if(color[n]==color[num]){
-                         return false;
-                    }
-
+    public boolean dfs(int node,int col,int [][]graph,int []color){
+        color[node]=col;
+                for(int i: graph[node]){
+                if(color[i]==-1){
+                    if(dfs(i,1-col,graph,color)==false) return false;
                 }
-               }
+                else if(color[i]==col){
+                    return false;
+                }
             }
-            }return true;
-        
+        return true;
     }
     public boolean isBipartite(int[][] graph) {
-        int []vis=new int[graph.length];
-        int []color=new int[graph.length];
-        for(int i=0;i<graph.length;i++){
-            if(vis[i]==0){
-                if(!bfs(i,vis,color,graph)){
+        int n=graph.length;
+        int []color=new int[n];
+        for(int i=0;i<n;i++){
+            color[i]=-1;
+        }
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(dfs(i,0,graph,color)==false){
                     return false;
                 }
             }
